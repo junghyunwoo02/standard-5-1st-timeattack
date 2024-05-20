46cc8d35d99cc773023ff0e78e5c57961c14a233
junghyunwoo02 <jungilbung@naver.com>
2024-05-20 15:52:17 +0900

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
+  const [state, setState ] = useState({
+
+  });
+  const [result, setResult] = useState(0);
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input type="number" value={} onChange={}/> 만큼을 <button>더할게요</button> <button>뺄게요</button>
         <button>초기화</button>
       </div>
       <hr />
